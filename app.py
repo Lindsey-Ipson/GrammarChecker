1b6be165bfb0f0073a4ff2a343acf056ba653bcd
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2023-08-16 15:22:28 -0700

    implement show all errors functionality

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request, flash, redirect, session, g, 
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
+from errors import generate_api_response, isolate_errors_from_api_response, add_errors_to_db, generate_grammar_errors_html, grammar_error_descriptions, apply_all_corrections, add_errors_to_db, generate_spelling_errors_html, add_text_to_db, order_error_types_by_frequency, create_show_all_grammar_errors_objects, get_misspelled_word_counts, create_spelling_error_html_objects
+
 from forms import SignupForm, LoginForm, SubmitTextForm
 from models import db, connect_db, User, Text
 
@@ -129,8 +131,6 @@ def logout():
 @app.route('/submit_text', methods=["GET", "POST"])
 def submit_text():
 
-    from errors import generate_api_response, isolate_errors_from_api_response, add_errors_to_db, generate_grammar_errors_html, grammar_error_descriptions, apply_all_corrections, add_errors_to_db, generate_spelling_errors_html, add_text_to_db
-
     form = SubmitTextForm()
 
     if not g.user:
@@ -143,33 +143,62 @@ def submit_text():
         
         text_to_submit = form.text.data
 
-        print('g.user', g.user)
+        api_response = generate_api_response(text_to_submit)
 
-        with app.app_context():
+        grammar_errors_list = isolate_errors_from_api_response(api_response, 'grammar')
+        spelling_errors_list = isolate_errors_from_api_response(api_response, 'spelling')
 
-            g.user = User.query.get(session[CURR_USER_KEY])
+        corrected_text = apply_all_corrections(text_to_submit, grammar_errors_list, spelling_errors_list)
 
-            api_response = generate_api_response(text_to_submit)
+        new_text = add_text_to_db(user.id, text_to_submit, corrected_text)
 
-            grammar_errors_list = isolate_errors_from_api_response(api_response, 'grammar')
-            spelling_errors_list = isolate_errors_from_api_response(api_response, 'spelling')
+        add_errors_to_db(grammar_errors_list, spelling_errors_list, user.id, new_text.id)
 
-            corrected_text = apply_all_corrections(text_to_submit, grammar_errors_list, spelling_errors_list)
+        html_grammar_errors = generate_grammar_errors_html(grammar_errors_list, grammar_error_descriptions)
+        html_spelling_errors = generate_spelling_errors_html(spelling_errors_list)
 
-            new_text = add_text_to_db(user.id, text_to_submit, corrected_text)
+        db.session.add(new_text)
+        db.session.commit()
 
-            add_errors_to_db(grammar_errors_list, spelling_errors_list, user.id, new_text.id)
+        flash("Text submitted successfully!", "success")
 
-            html_grammar_errors = generate_grammar_errors_html(grammar_errors_list, grammar_error_descriptions)
-            html_spelling_errors = generate_spelling_errors_html(spelling_errors_list)
-
-            db.session.add(new_text)
-            db.session.commit()
-
-            flash("Text submitted successfully!", "success")
-
-            print('g.user', g.user)
-
-            return render_template('review_text_submission.html', text=new_text, html_grammar_errors=html_grammar_errors, html_spelling_errors=html_spelling_errors) 
+        return render_template('review_text_submission.html', text=new_text, html_grammar_errors=html_grammar_errors,html_spelling_errors=html_spelling_errors) 
 
     return render_template('submit_text.html', form=form)
+
+
+@app.route('/show_all_grammar_errors', methods=["GET", "POST"])
+def show_all_grammar_errors():
+
+    user = g.user
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/signup")
+
+    error_types_ordered_by_frequency = order_error_types_by_frequency(user.id)
+
+    show_all_errors_objects = create_show_all_grammar_errors_objects(error_types_ordered_by_frequency)
+
+    return render_template('all_grammar_errors.html', all_grammar_errors=show_all_errors_objects, grammar_error_descriptions=grammar_error_descriptions)
+
+
+@app.route('/show_all_spelling_errors', methods=["GET", "POST"])
+def show_all_spelling_errors():
+
+    user = g.user
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/signup")
+
+    word_counts = get_misspelled_word_counts(user.id)
+
+    spelling_error_html_objects = create_spelling_error_html_objects(word_counts, user.id)
+
+    return render_template('all_spelling_errors.html', all_spelling_errors=spelling_error_html_objects)
+
+
+
+
+
--- errors.py
+++ errors.py
@@ -3,14 +3,13 @@ import requests
 import uuid
 import os
 from datetime import datetime
-
 from flask import session, g
+from sqlalchemy import func, desc
+
 from models import Grammar_Error, Spelling_Error, Text, db
-from app import app, CURR_USER_KEY
 
 
 API_key = os.environ.get("SAPLING_API_KEY") # to write in local machine: export SAPLING_API_KEY=value
-
 
 grammar_error_descriptions = {
     "M:PART": "Missing particle",
@@ -70,7 +69,6 @@ def generate_api_response(user_text):
     # Generate a random UUID (version 4) as a string
     uuid_string = str(uuid.uuid4())
 
-
     data_to_send = {
         "key": API_key,
         "text": user_text,
@@ -112,8 +110,6 @@ def isolate_errors_from_api_response(api_response_data, general_error_type):
 def add_errors_to_db(grammar_errors_list, spelling_errors_list, user_id, text_object_id): 
 # NOTE change text object?
         
-    with app.app_context():
-        
         for error in grammar_errors_list:
             new_grammar_error = Grammar_Error(
                 user_id=user_id,
@@ -140,22 +136,21 @@ def add_errors_to_db(grammar_errors_list, spelling_errors_list, user_id, text_ob
             db.session.commit()
 
 
-def add_spelling_errors_to_db(spelling_errors_list, user_id, text_object_id): 
-    with app.app_context():
+# def add_spelling_errors_to_db(spelling_errors_list, user_id, text_object_id): 
 
-        for error in spelling_errors_list:
+#     for error in spelling_errors_list:
     
-            new_grammar_error = Grammar_Error(
-                user_id=user_id,
-                text_id=text_object_id,
-                start=error['start'],
-                end=error['end'],
-                replacement=error['replacement'],
-                sentence=error['sentence']
-            )
+#         new_grammar_error = Grammar_Error(
+#             user_id=user_id,
+#             text_id=text_object_id,
+#             start=error['start'],
+#             end=error['end'],
+#             replacement=error['replacement'],
+#             sentence=error['sentence']
+#         )
     
-            db.session.add(new_grammar_error)
-            db.session.commit()
+#         db.session.add(new_grammar_error)
+#         db.session.commit()
 
 
 def add_text_to_db(user_id, text_to_submit, corrected_text):
@@ -173,50 +168,45 @@ def add_text_to_db(user_id, text_to_submit, corrected_text):
         return new_text
 
 
-def generate_grammar_errors_html(error_array, grammar_error_descriptions):
+def generate_grammar_errors_html(error_list, grammar_error_descriptions):
     html_errors_list = []
 
-    for error in error_array:
-
-        error_type = error["error_type"]
-        error_description = grammar_error_descriptions.get(error_type, "Unknown error")
-
-        sentence = error["sentence"]
-        error_start = error["start"]
-        error_end = error["end"]
+    for error in error_list:
 
         replacement = error["replacement"]
         if replacement == "":
             replacement = "(None - simply remove)"
 
-        html = f"""
-        <li>{error_description}</li>
-        <li class="hide-bullet">{sentence[:error_start]}<b>{sentence[error_start:error_end]}</b>{sentence[error_end:]}</li>
-        <li class="hide-bullet">Replacement: <b>{replacement}</b></li>
-        """
+        new_error_object = {
+            "error_description": grammar_error_descriptions.get(error["error_type"], "Unknown error"),
+            "sentence": error["sentence"],
+            "start": error["start"],
+            "end": error["end"],
+            "replacement": replacement
+        }
 
-        html_errors_list.append(html)
+        html_errors_list.append(new_error_object)
     
     return html_errors_list
 
 
-def generate_spelling_errors_html(error_array):
+def generate_spelling_errors_html(error_list):
     html_errors_list = []
 
-    for error in error_array:
-
-        sentence = error["sentence"]
-        error_start = error["start"]
-        error_end = error["end"]
+    for error in error_list:
 
         replacement = error["replacement"]
+        if replacement == "":
+            replacement = "(None - simply remove)"
 
-        html = f"""
-        <li>{sentence[:error_start]}<b>{sentence[error_start:error_end]}</b>{sentence[error_end:]}</li>
-        <li class="hide-bullet">Replacement: <b>{replacement}</b></li>
-        """
+        new_error_object = {
+            "sentence": error["sentence"],
+            "start": error["start"],
+            "end": error["end"],
+            "replacement": replacement
+        }
 
-        html_errors_list.append(html)
+        html_errors_list.append(new_error_object)
     
     return html_errors_list
 
@@ -233,5 +223,76 @@ def apply_all_corrections(text, grammar_errors_list, spelling_errors_list):
             continue
         text = text[: start] + edit['replacement'] + text[end:]
     return text
+
+
+def order_error_types_by_frequency(user_id):
+    print('getting error type counts for ', user_id)
+
+    error_types_and_counts = (
+        db.session.query(Grammar_Error.error_type, func.count (Grammar_Error.error_type))
+        .filter(Grammar_Error.user_id == user_id)
+        .group_by(Grammar_Error.error_type)
+        .order_by(func.count(Grammar_Error.error_type).desc())
+        .all()
+    )
+
+    result = [{"error_type": error_type, "count": count} for error_type, count in error_types_and_counts]
+
+    return result
+
+
+def get_grammar_errors_for_error_type(error_type):
+       
+    grammar_error_instances_list = (
+        Grammar_Error.query.filter(Grammar_Error.error_type == error_type, Grammar_Error.user_id == g.user.id)
+        .order_by(Grammar_Error.timestamp.desc())
+        .all()
+    )
+
+    return grammar_error_instances_list
+
+
+def create_show_all_grammar_errors_objects(error_types_ordered_by_frequency):
+    show_all_errors_objects = []
+
+    for error_type_object in error_types_ordered_by_frequency:
+        error_type_object['errors'] = get_grammar_errors_for_error_type(error_type_object['error_type'])
+        show_all_errors_objects.append(error_type_object)
+
+    return show_all_errors_objects
+
+
+def get_misspelled_word_counts(user_id):
+
+    word_counts = db.session.query(
+        Spelling_Error.replacement,
+        func.count(Spelling_Error.replacement).label('count')
+    ).filter(Spelling_Error.user_id == user_id).group_by(Spelling_Error.replacement).order_by(func.count(Spelling_Error.replacement).desc()).all()
+
+    return word_counts
+
+
+def get_misspelled_errors_for_word(word, user_id):
+    # Query to get a list of misspelled errors for a specific word
+    errors = (
+        Spelling_Error.query.filter(Spelling_Error.user_id == user_id, Spelling_Error.replacement == word)
+    .order_by(Spelling_Error.timestamp.desc())
+    .all()
+    )
+
+    return errors
+
+
+def create_spelling_error_html_objects(word_counts, user_id):
+    # Create a list of objects for each misspelled word
+    spelling_errors = []
+    for word, count in word_counts:
+        new_object = {"word": word, "count": count}
+        errors_for_word = get_misspelled_errors_for_word(word, user_id)
+        new_object["misspellings"] = errors_for_word
+        spelling_errors.append(new_object)
+
+    return spelling_errors
+
 
 
