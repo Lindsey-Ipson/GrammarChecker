97931bd6a998389f62f03dfd9a4c9004384d00bf
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2023-09-06 14:48:16 -0700

    change app.config['DEBUG'] to check for environmental variable first

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-app.config['DEBUG'] = True
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', True)
 
 toolbar = DebugToolbarExtension(app)
 
